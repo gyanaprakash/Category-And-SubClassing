1f21815df07ace2313c06383524cdafe8e7e2b24
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-12-15 12:26:06 +0530

    Initial Commit

---
+++ YAACCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YAACCategory
+//
+//  Created by Bsetecip10 on 15/12/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YAACCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAACCategory
+//
+//  Created by Bsetecip10 on 15/12/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
